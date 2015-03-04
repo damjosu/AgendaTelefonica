457a62dae364516271adacd86e359e07f0266456
damjosu <damjosu@gmail.com>
2015-03-04 10:47:42 +0100

    documentación creada

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -9,25 +9,35 @@ public class AgendaTelefonica
 {
     private HashMap<String, String> agenda;
     /**
-     * Constructor for objects of class AgendaTelefonica
+     * Constructor for objects of class AgendaTelefonica     * 
      */
     public AgendaTelefonica()
     {
         agenda = new HashMap<>();
     }
 
+    /**
+     * Introduce una persona y número en la agenda.
+     * @param name El nombre de la persona
+     * @param number El número asociado a la persona.
+     */
     public void enterNumber(String name, String number)
     {
         agenda.put(name, number);
     }
 
+    /**
+     * Busca por un nombre en la agenda.
+     * @param name El nombre por el que buscas.
+     * @return agenda.get(name) El número asociado al nombre.
+     */
     public String lookupNumber(String name)
     {
         return agenda.get(name);
     }
 
     /**
-     * Print all keys
+     * Muestra todos los nombres. 
      */
     public void printAllNames()
     {
